854ceab9adce8b25d829b3f82b88c3b06eb60235
lucas.coelho <ludico1959@gmail.com>
2022-04-17 01:02:23 -0300

    classes from 4 to 5 from ava chapter 4

--- Cap04 - Imutabilidade, representação e formato, coesão/App03.java
+++ Cap04 - Imutabilidade, representação e formato, coesão/App03.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class App03 {
   public static void main(String[] args) {
     // imutabilidade
@@ -9,10 +11,32 @@ public class App03 {
     System.out.println(cinza02.equals(cinza03)); // false
     System.out.println(cinza01.equals(cinza03)); // false
 
-    Cor teste01 = new Cor(100, 50, 50);
-    System.out.println(teste01.red); // 100
+    Cor testeDeCor = new Cor(100, 50, 50);
+    System.out.println(testeDeCor.red); // 100
 
-    Cor teste02 = teste01.avermelhar();
-    System.out.println(teste02.red); // 101
+    Cor outroTesteDeCor = testeDeCor.avermelhar();
+    System.out.println(outroTesteDeCor.red); // 101
+
+    // Representação e Formato:
+    int[] corArray1 = outroTesteDeCor.toArray();
+    System.out.println(Arrays.toString(corArray1)); // [101, 50, 50]
+
+    System.out.println(testeDeCor.toMap()); // {red=100, green=50, blue=50}
+
+    int[] cores = { 120, 67, 13 };
+    Cor corArray2 = new Cor(cores);
+
+    System.out.println(corArray2.red); // 120
+
+    // Contexto estático e métodos de fábrica:
+    System.out.println(Cor.fromArray(new int[] {12, 65, 178})); // Cor@f73
+
+    Cor corHex = Cor.fromHexString("#ADFF2F"); // rgb(173, 255, 47)
+    System.out.println(corHex.blue); // 47
+
+    Cor corName = Cor.fromName("indigo");
+    System.out.println(corName.blue); // 130
+
+    System.out.println(Cor.COR_PRETA); // Cor@0
   }
 }
--- Cap04 - Imutabilidade, representação e formato, coesão/Cor.java
+++ Cap04 - Imutabilidade, representação e formato, coesão/Cor.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import static java.lang.Integer.parseInt;
+
 /** Tornando Cor um OBJETO DE VALOR:
  * 1. Tornar os objetos constantes (final / readonly)
  * 2. Sempre há um construtor que inicialize as constantes
@@ -6,22 +10,36 @@
 */
 
 public class Cor {
+  // constante de classe: objetos pré-instanciados
+  // semelhante ao método Math.PI
+  public static Cor COR_PRETA  = new Cor(0, 0, 0);
+  public static Cor COR_BRANCA = new Cor(255, 255, 255);
+
   // atributos constantes ✔
   public final int red;
   public final int blue;
   public final int green;
 
   // construtor que inicialize as constantes ✔
+  public Cor() {
+    this(0, 0, 0);
+  }
+
   public Cor (int red, int green, int blue) {
     if (red < 0 || green < 0 || blue < 0)
-      throw new IllegalArgumentException("Há valor RGB menor que 0");
+      throw new IllegalArgumentException("há valor RGB menor que 0");
     
     if (red > 255 || green > 255 || blue > 255)
-      throw new IllegalArgumentException("Há valor RGB maior que 255");
+      throw new IllegalArgumentException("há valor RGB maior que 255");
 
     this.red = red;
     this.green = green;
     this.blue = blue;
+  }
+
+  // sobrecarga de construtor para aceitar um array de inteiros
+  public Cor (int[] array) {
+    this(array[0], array[1], array[2]);
   }
 
   // adicionar comandos que retirnam uma nova instância ✔
@@ -29,6 +47,54 @@ public class Cor {
   public Cor avermelhar() {
     return new Cor(this.red + 1, this.green, this.blue);
   }
+
+  //////////////////////////////////////////////////////////
+  // convenção de métodos toTipo e fromTipo
+  public int[] toArray() { // var.toArray
+    return new int[] {
+      this.red,
+      this.green,
+      this.blue
+    };
+  }
+
+  // contexto estático: 
+  // os métodos 'from' tipicamento são codados como métodos fábrica esstáticos
+  public static Cor fromArray(int[] array) { // Color.fromArray
+    return new Cor(array[0], array[1], array[2]);
+  }
+                                                      //0123456
+  public static Cor fromHexString(String str) { // ex.: #CD5C5C
+    String r = str.substring(1, 3);
+    String g = str.substring(3, 5);
+    String b = str.substring(5, 7);
+
+    return new Cor(parseInt(r, 16), parseInt(g, 16), parseInt(b, 16));
+  }
+
+  public static Cor fromName(String nome) {
+    switch (nome.toLowerCase()) {
+      case "black":   return new Cor(0, 0, 0);
+      case "silver":  return new Cor(192, 192, 192);
+      case "purple":  return new Cor(128, 0, 128);
+      case "fuchsia": return new Cor(255, 0, 255);
+      case "green":   return new Cor(0, 128, 0);
+      case "lime":    return new Cor(0, 255, 0);
+      case "indigo":  return new Cor(75, 0, 130);
+    }
+
+    throw new IllegalArgumentException("não identificamos esse nome de cor");
+  }
+
+  public Map<String, Integer> toMap() {
+    Map<String, Integer> mapa = new HashMap<>();
+    mapa.put("red", this.red);
+    mapa.put("blue", this.blue);
+    mapa.put("green", this.green);
+
+    return mapa;
+  }
+  //////////////////////////////////////////////////////////
 
   // Sobrescrever o método'equals' ✔
   @Override
@@ -43,6 +109,7 @@ public class Cor {
 
     return false;
   }
+  
 
   // Não é relevante no momento...
   @Override
